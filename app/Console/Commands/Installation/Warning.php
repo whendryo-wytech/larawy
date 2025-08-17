14c819cb95e7305be922184a5d6022f621629424
Whendryo <whendryo@wytech.com.br>
2025-08-17 09:36:48 -0300

    Initial commit

---
+++ app/Console/Commands/Installation/Warning.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands\Installation;
+
+use Illuminate\Console\Command;
+
+class Warning extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'installation:warning';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Post-installation warning for the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->warn('Run the following command to complete the installation:');
+        $this->line('php artisan installation:post');
+    }
+}
